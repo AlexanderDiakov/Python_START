3962f0900a09d26bd26a91218e8afdff0f2ee5ce
Alexander Diakov <alex.dkv@yandex.ru>
2022-12-03 03:00:25 +0300

    task_1 done

--- HW_3/task_1.py
+++ HW_3/task_1.py
@@ -11,3 +11,14 @@
 [5, 1, 5, 2, 7, 11]
 14
 """
+
+import random
+
+num = int(input('Введите число '))
+
+new_list = [random.randint(1, num) for i in range(num)]
+sum_odds = 0
+for i in range(1, num, 2):
+    sum_odds += new_list[i]
+print(new_list)
+print(sum_odds)
