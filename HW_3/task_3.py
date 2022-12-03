a8803126d474a3e23ba7d30548bfa7b7a087255f
Alexander Diakov <alex.dkv@yandex.ru>
2022-12-03 03:22:16 +0300

    task_3 done

--- HW_3/task_3.py
+++ HW_3/task_3.py
@@ -9,3 +9,8 @@
 [1.1, 1.2, 3.1, 5, 10.01]
 2.0
 """
+
+first_list = list(map(float, input("Введите числа через пробел: ").split()))
+
+new_list = [round(i % 1, 2) for i in first_list if i % 1 != 0]
+print(max(new_list) - min(new_list))
