8a48a3e8f0e2af590425ccc20fb4302fe6d3bec2
Alexander Diakov <alex.dkv@yandex.ru>
2022-12-03 03:28:55 +0300

    task_5 done

--- HW_3/task_5.py
+++ HW_3/task_5.py
@@ -10,3 +10,14 @@ https://ru.wikipedia.org/wiki/Негафибоначчи
 8
 [-21 ,13, -8, 5, −3, 2, −1, 1, 0, 1, 1, 2, 3, 5, 8, 13, 21]
 """
+
+num = int(input("Введите число N: "))
+
+fib_list = [0] * (2 * num + 1)
+fib_list[num + 1], fib_list[num - 1] = 1, 1
+coef = -1
+for i in range(num + 2, 2 * num + 1):
+    fib_list[i] = fib_list[i - 1] + fib_list[i - 2]
+    fib_list[-i - 1] = fib_list[i] * coef
+    coef *= -1
+print(fib_list)
