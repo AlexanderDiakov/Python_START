346b1fd731522c33fb546c0bc564e115167771f0
Alexander Diakov <alex.dkv@yandex.ru>
2022-12-15 07:31:22 +0300

    task_2 done

--- HW_6/task_2.py
+++ HW_6/task_2.py
@@ -13,3 +13,22 @@
 [1, 3, 5]
 [1, 2, 5, 3, 10]
 """
+
+
+new_list = [1, 2, 3, 5, 1, 5, 3, 10]
+
+unique = [i for i in new_list if new_list.count(i) == 1]
+
+repeatable_list = [i for i in new_list if (new_list.count(i)) > 1]
+temp = []
+for i in repeatable_list:
+    if i not in temp:
+        temp.append(i)
+    repeatable_list = temp
+
+without_rep = list(set(new_list))
+
+print(new_list)
+print(unique)
+print(temp)
+print(without_rep)
